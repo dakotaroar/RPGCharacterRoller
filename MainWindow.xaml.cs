16b7cbc8ddd2daa8cfb9e8627c88ca4a8646ff59
802630ctc <802630ctc@MA-CTC-INST-S04>
2023-03-10 12:05:25 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPGCharacterRoller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RPGCharacter _character = new RPGCharacter();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            updateStats();
+            
+        }
+
+        private void buttonUpdateName_Click(object sender, RoutedEventArgs e)
+        {
+            _character.Name = textBoxName.Text;
+        }
+
+        private void buttonReroll_Click(object sender, RoutedEventArgs e)
+        {
+            _character.Roll();
+            updateStats();
+
+        }
+
+        private void updateStats()
+        {
+            textStrength.Text = _character.Strength.ToString();
+            textIntelligence.Text = _character.Intelligence.ToString();
+            textDexterity.Text = _character.Dexterity.ToString();
+            textCharisma.Text = _character.Charisma.ToString();
+            textStamina.Text= _character.Stamina.ToString();
+            textWisdom.Text= _character.Wisdom.ToString();
+        }
+    }
+}
---
+++ RPGCharacter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGCharacterRoller
+{
+    internal class RPGCharacter
+    {
+        // Fields
+        private int _strength;
+        private int _intelligence;
+        private int _wisdom;
+        private int _dexterity;
+        private int _stamina;
+        private int _charisma;
+        private int _maxCharisma = 20;
+        private int _maxDexterity = 20;
+        private int _maxIntelligence = 20;
+        private int _maxStamina = 20;
+        private int _maxStrength = 20;
+        private int _maxWisdom = 20;
+
+        private CharacterClasses _characterClasses = CharacterClasses.None;
+        private Random _rng = new Random();
+
+        #region Properties
+        public string Name { get; set; }
+        public long XP {  get; set; }
+        public int Level { get; set; }
+        public CharacterClasses CharacterClass
+        {
+            get { return _characterClasses; }
+            set { _characterClasses = value; }
+        }
+        public int Charisma { get { return _charisma; } }
+        public int Dexterity { get { return _dexterity; } }
+        public int Intelligence { get { return _intelligence; } }
+        public int Stamina { get { return _stamina; } }
+        public int Strength { get { return _strength; } }
+        public int Wisdom { get { return _wisdom; } }
+        #endregion
+
+        public RPGCharacter()
+        {
+            Roll();
+        }
+
+        public void Roll()
+        {
+            _charisma = _rng.Next(1, _maxCharisma + 1);
+            _dexterity = _rng.Next(1, _maxDexterity + 1);
+            _intelligence = _rng.Next(1, _maxIntelligence + 1);
+            _stamina = _rng.Next(1, _maxStamina + 1);
+            _strength = _rng.Next(1, _maxStrength + 1);
+            _wisdom = _rng.Next(1, _maxWisdom + 1);
+        }
+
+    }
+
+    public enum CharacterClasses
+    {
+        None = 0,
+        Ranger,
+        Mage,
+        Rogue,
+        Bard,
+        Goliath,
+        Paladin,
+    }
+}
